65592fdbdb052fba2e5b83aeece930d3323e25c4
rpetrietc <94928365+rpetrietc@users.noreply.github.com>
2022-11-23 16:01:37 -0500

    Code to make some FetchXML calls for Region and Stakeholders

--- Program.cs
+++ Program.cs
@@ -23,7 +23,68 @@ namespace ROM.TSIS2.CSharpAPIDemo
             // Connect to the TSIS 2 - ROM API
             using (var svc = new CrmServiceClient(connectString))
             {
+                // EXAMPLE - Retrieve all Regions in English
+                {
+                    EntityCollection regions = svc.RetrieveMultiple(new FetchExpression(FetchXMLExamples.All_Regions_English));
+
+                    Console.WriteLine("All Regions in English:");
+                    Console.WriteLine();
+
+                    foreach (var region in regions.Entities)
+                    {
+                        Console.WriteLine($"ID: {region.Id.ToString()}");
+                        Console.WriteLine($"Name: {region.GetAttributeValue<String>("ovs_territorynameenglish")}");
+                        Console.WriteLine();
+                    }
+                }
+
+                // EXAMPLE - Retrieve all Regions in French
+                {
+                    EntityCollection regions = svc.RetrieveMultiple(new FetchExpression(FetchXMLExamples.All_Regions_French));
+
+                    Console.WriteLine("All Regions in French:");
+                    Console.WriteLine();
+
+                    foreach (var region in regions.Entities)
+                    {
+                        Console.WriteLine($"ID: {region.Id.ToString()}");
+                        Console.WriteLine($"Name: {region.GetAttributeValue<String>("ovs_territorynamefrench")}");
+                        Console.WriteLine();
+                    }
+                }
+
+                // EXAMPLE - Retrieve all Aviation Security Stakeholders
+                {
+                    EntityCollection stakeholders = svc.RetrieveMultiple(new FetchExpression(FetchXMLExamples.All_Operational_AvSec_Stakeholders));
+
+                    Console.WriteLine("All AvSec Stakeholders:");
+                    Console.WriteLine();
+
+                    foreach (var stakeholder in stakeholders.Entities)
+                    {
+                        Console.WriteLine($"ID: {stakeholder.Id.ToString()}");
+                        Console.WriteLine($"Name: {stakeholder.GetAttributeValue<String>("name")}");
+                        Console.WriteLine();
+                    }
+                }
+
+                // EXAMPLE - Retrieve all Intermodal Surface Security Oversight Stakeholders
+                {
+                    EntityCollection stakeholders = svc.RetrieveMultiple(new FetchExpression(FetchXMLExamples.All_Operational_ISSO_Stakeholders));
+
+                    Console.WriteLine("All ISSO Stakeholders:");
+                    Console.WriteLine();
+
+                    foreach (var stakeholder in stakeholders.Entities)
+                    {
+                        Console.WriteLine($"ID: {stakeholder.Id.ToString()}");
+                        Console.WriteLine($"Name: {stakeholder.GetAttributeValue<String>("name")}");
+                        Console.WriteLine();
+                    }
+                }
             }
+
+            Console.Read();
         }
     }
 }
