3cc49354d3e54e138ae3b272c2659861bdeb34e4
rpetrietc <94928365+rpetrietc@users.noreply.github.com>
2022-12-29 11:31:19 -0500

    Clean up LookupColumns, use a struct instead of an enum

--- CSharpAPIDemo-NetCore/LookupColumns.cs
+++ CSharpAPIDemo-NetCore/LookupColumns.cs
@@ -6,12 +6,6 @@ namespace CSharpAPIDemo_NetCore
 {
     public static class LookupColumns
     {
-        //public enum ts_securityincidenttype
-        //{
-        //    ts_securityincidenttypenameenglish,
-        //    ts_securityincidenttypenamefrench
-        //}
-
         public struct SecurityIncidentType
         {
             public const string EnglishName = "ts_securityincidenttypenameenglish";
