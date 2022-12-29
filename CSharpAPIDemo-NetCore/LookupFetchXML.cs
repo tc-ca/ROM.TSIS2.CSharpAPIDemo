7ba9126e57f11de687802e2a45e1c7772ae87b5e
rpetrietc <94928365+rpetrietc@users.noreply.github.com>
2022-12-29 16:25:32 -0500

    Code on how to retrieve all lookups

--- CSharpAPIDemo-NetCore/LookupFetchXML.cs
+++ CSharpAPIDemo-NetCore/LookupFetchXML.cs
@@ -15,7 +15,7 @@ namespace CSharpAPIDemo_NetCore
                           <entity name='ts_securityincidenttype'>
                             <attribute name='ts_securityincidenttypenameenglish' />
                             <filter>
-                              <condition attribute='statecode' operator='eq' value='0' />
+                              <condition attribute='statuscode' operator='eq' value='1' />
                             </filter>
                             <order attribute='ts_securityincidenttypenameenglish' />
                           </entity>
@@ -29,7 +29,7 @@ namespace CSharpAPIDemo_NetCore
                           <entity name='ts_securityincidenttype'>
                             <attribute name='ts_securityincidenttypenamefrench' />
                             <filter>
-                              <condition attribute='statecode' operator='eq' value='0' />
+                              <condition attribute='statuscode' operator='eq' value='1' />
                             </filter>
                             <order attribute='ts_securityincidenttypenamefrench' />
                           </entity>
@@ -37,5 +37,131 @@ namespace CSharpAPIDemo_NetCore
                 ";
             }
         }
+
+        public static string TargetElements(bool english = true)
+        {
+            if (english)
+            {
+                return @"
+                        <fetch>
+                          <entity name='ts_targetelement'>
+                            <attribute name='ts_targetelementnameenglish' />
+                            <filter>
+                              <condition attribute='statuscode' operator='eq' value='1' />
+                            </filter>
+                            <order attribute='ts_targetelementnameenglish' />
+                          </entity>                        
+                        </fetch>
+                ";
+            }
+            else
+            {
+                return @"
+                        <fetch>
+                          <entity name='ts_targetelement'>
+                            <attribute name='ts_targetelementnamefrench' />
+                            <filter>
+                              <condition attribute='statuscode' operator='eq' value='1' />
+                            </filter>
+                            <order attribute='ts_targetelementnamefrench' />
+                          </entity>                        
+                        </fetch>
+                ";
+            }
+        }
+
+        // Reporting Company and Stakeholder reside in the same table in ROM
+        public static string ReportingCompany()
+        {
+            return @"
+                    <fetch>
+                        <entity name='account'>
+                        <attribute name='name' />
+                        <filter>
+                            <condition attribute='statuscode' operator='eq' value='1' />
+                        </filter>
+                        <order attribute='name' />
+                        </entity>
+                    </fetch>
+            ";
+        }
+
+        public static string StakeholderOperationType(bool english = true)
+        {
+            if (english)
+            {
+                return @"
+                        <fetch>
+                          <entity name='ovs_operationtype'>
+                            <attribute name='ovs_operationtypenameenglish' />
+                            <filter>
+                              <condition attribute='statuscode' operator='eq' value='1' />
+                            </filter>
+                            <order attribute='ovs_operationtypenameenglish' />
+                          </entity>
+                        </fetch>
+                ";
+            }
+            else
+            {
+                return @"
+                        <fetch>
+                          <entity name='ovs_operationtype'>
+                            <attribute name='ovs_operationtypenamefrench' />
+                            <filter>
+                              <condition attribute='statuscode' operator='eq' value='1' />
+                            </filter>
+                            <order attribute='ovs_operationtypenamefrench' />
+                          </entity>
+                        </fetch>
+                ";
+            }
+        }
+
+        public static string Region(bool english = true)
+        {
+            if (english)
+            {
+                return @"
+                        <fetch>
+                          <entity name='territory'>
+                            <attribute name='ovs_territorynameenglish' />
+                            <order attribute='ovs_territorynameenglish' />
+                          </entity>
+                        </fetch>
+                ";
+            }
+            else
+            {
+                return @"
+                        <fetch>
+                          <entity name='territory'>
+                            <attribute name='ovs_territorynamefrench' />
+                            <order attribute='ovs_territorynamefrench' />
+                          </entity>
+                        </fetch>
+                ";
+            }
+        }
+
+        // Site, SubSite, Origin, and Destination reside in the same table in ROM
+        public static string Site()
+        {
+            return @"
+                    <fetch>
+                        <entity name='msdyn_functionallocation'>
+                        <attribute name='msdyn_name' />
+                        <attribute name='msdyn_stateorprovince' />
+                        <attribute name='msdyn_longitude' />
+                        <attribute name='msdyn_latitude' />
+                        <filter>
+                            <condition attribute='statuscode' operator='eq' value='1' />
+                        </filter>
+                        <order attribute='msdyn_name' />
+                        </entity>
+                    </fetch>
+            ";
+        }
+
     }
 }
--- CSharpAPIDemo-NetCore/Program.cs
+++ CSharpAPIDemo-NetCore/Program.cs
@@ -34,18 +34,123 @@ namespace CSharpAPIDemo_NetCore
                     // English
                     EntityCollection securityIncidentTypesEnglish = svc.RetrieveMultiple(new FetchExpression(LookupFetchXML.SecurityIncidents()));
 
-                    // French
+                    // French - Use this to retrieve the French names
                     EntityCollection securityIncidentTypesFrench = svc.RetrieveMultiple(new FetchExpression(LookupFetchXML.SecurityIncidents(false)));
+
+                    Console.WriteLine("Security Incident Types:");
 
                     foreach (var securityIncident in securityIncidentTypesEnglish.Entities)
                     {
                         Console.WriteLine($"ID: {securityIncident.Id.ToString()}");
                         Console.WriteLine($"Name: {securityIncident.GetAttributeValue<String>(LookupColumns.SecurityIncidentType.EnglishName)}");
+                        Console.WriteLine("");
                     }
-                }
-            }
 
-            Console.WriteLine("Hello World!");
+                    Console.WriteLine("End of Security Incident Types-------------------");
+                }
+
+                // EXAMPLE - Retrieve all Target Elements
+                {
+                    // English
+                    EntityCollection targetElementEnglish = svc.RetrieveMultiple(new FetchExpression(LookupFetchXML.TargetElements()));
+
+                    // French - Use this to retrieve the French names
+                    EntityCollection targetElementFrench = svc.RetrieveMultiple(new FetchExpression(LookupFetchXML.TargetElements(false)));
+
+                    Console.WriteLine("Target Elements:");
+
+                    foreach (var targetElement in targetElementEnglish.Entities)
+                    {
+                        Console.WriteLine($"ID: {targetElement.Id.ToString()}");
+                        Console.WriteLine($"Name: {targetElement.GetAttributeValue<String>(LookupColumns.TargetElement.EnglishName)}");
+                        Console.WriteLine("");
+                    }
+
+                    Console.WriteLine("End of Target Elements-------------------");
+                }
+
+                // EXAMPLE - Retrieve all Reporting Companies
+                // Reporting Company and Stakeholder reside in the same table in ROM
+                {
+                    // English
+                    EntityCollection reportingCompany = svc.RetrieveMultiple(new FetchExpression(LookupFetchXML.ReportingCompany()));
+
+                    Console.WriteLine("Reporting Companies:");
+
+                    foreach (var reportingcompany in reportingCompany.Entities)
+                    {
+                        Console.WriteLine($"ID: {reportingcompany.Id.ToString()}");
+                        Console.WriteLine($"Name: {reportingcompany.GetAttributeValue<String>(LookupColumns.ReportingCompany.Name)}");
+                        Console.WriteLine("");
+                    }
+
+                    Console.WriteLine("End of Reporting Companies-------------------");
+                }
+
+                // EXAMPLE - Retrieve all Stakeholder Operation Types
+                {
+                    // English
+                    EntityCollection stakeholderOperationTypesEnglish = svc.RetrieveMultiple(new FetchExpression(LookupFetchXML.StakeholderOperationType()));
+
+                    // French - Use this to retrieve the French names
+                    EntityCollection stakeholderOperationTypesFrench = svc.RetrieveMultiple(new FetchExpression(LookupFetchXML.TargetElements(false)));
+
+                    Console.WriteLine("Stakeholder Operation Types:");
+
+                    foreach (var stakeholderOperationType in stakeholderOperationTypesEnglish.Entities)
+                    {
+                        Console.WriteLine($"ID: {stakeholderOperationType.Id.ToString()}");
+                        Console.WriteLine($"Name: {stakeholderOperationType.GetAttributeValue<String>(LookupColumns.StakeholderOperationType.EnglishName)}");
+                        Console.WriteLine("");
+                    }
+
+                    Console.WriteLine("End of Stakeholder Operation Types-------------------");
+                }
+
+                // EXAMPLE - Retrieve all Regions
+                {
+                    // English
+                    EntityCollection regionsEnglish = svc.RetrieveMultiple(new FetchExpression(LookupFetchXML.Region()));
+
+                    // French - Use this to retrieve the French names
+                    EntityCollection regionsFrench = svc.RetrieveMultiple(new FetchExpression(LookupFetchXML.Region(false)));
+
+                    Console.WriteLine("Regions:");
+
+                    foreach (var region in regionsEnglish.Entities)
+                    {
+                        Console.WriteLine($"ID: {region.Id.ToString()}");
+                        Console.WriteLine($"Name: {region.GetAttributeValue<String>(LookupColumns.Region.EnglishName)}");
+                        Console.WriteLine("");
+                    }
+
+                    Console.WriteLine("End of Regions-------------------");
+                }
+
+                // EXAMPLE - Retrieve all Sites
+                // Site, SubSite, Origin, and Destination reside in the same table in ROM
+                {
+                    // English
+                    EntityCollection sites = svc.RetrieveMultiple(new FetchExpression(LookupFetchXML.Site()));
+
+                    Console.WriteLine("Sites:");
+
+                    foreach (var site in sites.Entities)
+                    {
+                        Console.WriteLine($"ID: {site.Id.ToString()}");
+                        Console.WriteLine($"Name: {site.GetAttributeValue<String>(LookupColumns.Site.Name)}");
+                        Console.WriteLine($"Province: {site.GetAttributeValue<String>(LookupColumns.Site.Province)}");
+                        Console.WriteLine($"Longitude: {site.GetAttributeValue<double>(LookupColumns.Site.Longitude).ToString()}");
+                        Console.WriteLine($"Latitude: {site.GetAttributeValue<double>(LookupColumns.Site.Latitude).ToString()}");
+                        Console.WriteLine("");
+                    }
+
+                    Console.WriteLine("End of Sites-------------------");
+                }
+
+                Console.WriteLine("- End Of Program -");
+                Console.ReadLine();
+            }
         }
     }
 
